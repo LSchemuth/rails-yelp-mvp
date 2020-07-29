b4d14180cc7c68dc452521e4fddab08d23b74dc1
Luissa Schemuth <luissa.schemuth@outlook.com>
2020-07-29 12:56:31 +0200

    made everything work

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :restaurants, only: [:new, :create, :show] do # Parent segment
+  resources :restaurants, only: [:index, :new, :create, :show] do # Parent segment
     # GET /restaurants/:restaurant_id/reviews/new
     resources :reviews, only: [:new, :create] # Child segment
   end
